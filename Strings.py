2208351dec2e5562b0f561c5ceafc6eb02f0fe9f
Eduardo110 <51434463+Eduardo110@users.noreply.github.com>
2019-06-06 02:15:32 -0500

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,19 @@
+print("This is text")
+
+print('This is text')
+
+print("'This is text'")
+
+print('"This is text"')
+
+print("""this
+is
+text""")
+
+#print('This won't work') Este marca error
+
+print('This won\'t error')
+
+print('something\new')
+
+print(r'something\new')
