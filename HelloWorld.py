4768e90ee7724f9de399f323cb9391a1caecbfb7
Eduardo110 <51434463+Eduardo110@users.noreply.github.com>
2019-06-06 02:14:44 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+message = "Hello World"
+print(message)
+
+# create a variable to hold the text
+message = "Hello World"
+# print the text stored in the variable
+print(message)
