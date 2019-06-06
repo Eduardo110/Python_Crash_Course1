418e83e1c54ad6c2106a57d5d755e4e4516609e6
Eduardo110 <51434463+Eduardo110@users.noreply.github.com>
2019-06-06 02:14:49 -0500

    Create ImportingUsingModules.py

---
+++ ImportingUsingModules.py
@@ -0,0 +1,16 @@
+from math import sqrt
+print(sqrt(9))
+
+import math as m
+print(m.sqrt(9))
+
+from math import sqrt as s
+print(s(9))
+
+
+#Built-in Modules
+import sys
+sys.builtin_module_names
+print (sys.builtin_module_names)
+
+help('modules')
